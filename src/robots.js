13f16992097d351f37d9d1563ec7bdd8d60fa204
Nader Nasr <nadernasr7@gmail.com>
2020-02-19 14:30:08 -0500

    search app

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'nader nasr',
+    username: 'Nader',
+    email: 'nadernasr.io'
+  },
+  {
+    id: 2,
+    name: 'jack johns',
+    username: 'jay jay',
+    email: 'jj.io'
+  },
+  {
+    id: 3,
+    name: 'mousa',
+    username: 'mousa',
+    email: 'moumou.io'
+  },
+  {
+    id: 4,
+    name: 'gaby',
+    username: 'gab',
+    email: 'gaby.io'
+  },
+  {
+    id: 5,
+    name: 'john',
+    username: 'jogn',
+    email: 'joghn.io'
+  },
+  {
+    id: 6,
+    name: 'gabt',
+    username: 'gab',
+    email: 'gaby.io'
+  },
+  {
+    id: 7,
+    name: 'gabr',
+    username: 'gab',
+    email: 'gaby.io'
+  },
+  {
+    id: 8,
+    name: 'gabq',
+    username: 'gab',
+    email: 'gaby.io'
+  },
+  {
+    id: 9,
+    name: 'gabb',
+    username: 'gab',
+    email: 'gaby.io'
+  }
+]
