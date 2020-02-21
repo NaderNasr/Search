6df064ef6599459dc5e48365a7a351ca08720145
Nader Nasr <nadernasr7@gmail.com>
2020-02-20 19:46:10 -0500

    clean up

---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import Loader from 'react-loader-spinner'
+import Scroll from '../components/Scroll'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({robots: users}))
+
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value});
+
+  }
+  render() {
+    const {robots, searchfield} = this.state;
+    const filterRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    if (robots === 0) {
+      return <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={3000}/>
+    } else {
+      return (<div className='tc'>
+        <h1>RoboFriends</h1>
+
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
+          <CardList robots={filterRobots}/>
+        </Scroll>
+      </div>)
+    }
+  }
+}
+
+export default App;
